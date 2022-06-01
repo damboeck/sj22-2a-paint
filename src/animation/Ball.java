361aa16774190ab4ea7c2e9561ec3a570a1a6237
Werner <werner.damboeck@letto.at>
2022-06-01 11:49:27 +0200

    Ballspiel ohne Schläger

--- src/animation/Ball.java
+++ src/animation/Ball.java
@@ -7,10 +7,51 @@ import java.awt.*;
 public class Ball implements Paintable {
     private double size;
     private Color  color;
-    private Vect2D position;
+    private Vect2D p;
+    private Vect2D v;
+
+    public Ball(double size, Color color, double x, double y, double vx, double vy) {
+        this.size = size;
+        this.color= color;
+        this.p    = new Vect2D(x,y);
+        this.v    = new Vect2D(vx,vy);
+    }
 
     @Override
     public void paint(Graphics g) {
+        g.setColor(color);
+        g.fillOval((int)(p.x-size/2),(int)(p.y-size/2),(int)size,(int)size);
+    }
 
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Vect2D getP() {
+        return p;
+    }
+
+    public void setP(Vect2D p) {
+        this.p = p;
+    }
+
+    public Vect2D getV() {
+        return v;
+    }
+
+    public void setV(Vect2D v) {
+        this.v = v;
     }
 }
--- src/animation/Ballspiel.java
+++ src/animation/Ballspiel.java
@@ -5,8 +5,11 @@ import java.awt.event.KeyEvent;
 
 public class Ballspiel extends Animation {
 
+    Ball ball;
+
     public Ballspiel(String title, int width, int height) {
         super(title, width, height);
+        ball = new Ball(50,Color.red,100,100,20,5);
         setVisible(true);
     }
 
@@ -16,7 +19,15 @@ public class Ballspiel extends Animation {
 
     @Override
     public void paint(Graphics g) {
-
+        // Physik berechnen
+        ball.setP(ball.getP().add(ball.getV()));
+        // Wände
+        if (ball.getP().x>800-ball.getSize()/2) ball.setV(new Vect2D(-ball.getV().x,ball.getV().y));
+        if (ball.getP().x<ball.getSize()/2) ball.setV(new Vect2D(-ball.getV().x,ball.getV().y));
+        if (ball.getP().y>600-ball.getSize()/2) ball.setV(new Vect2D(ball.getV().x,-ball.getV().y));
+        if (ball.getP().y<ball.getSize()/2) ball.setV(new Vect2D(ball.getV().x,-ball.getV().y));
+        // Darstellung
+        ball.paint(g);
     }
 
     @Override
